e368f670b398d2343d1c2c379e450f2dff8cf41e
AIIrondev <iron.ai.dev@gmail.com>
2025-03-10 22:58:53 +0100

    Implement new Flask application structure with user authentication, registration, and chat functionality; add templates, styles, and scripts for improved user interface

---
+++ DeploymentCenter/app.py
@@ -0,0 +1,62 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory, get_flashed_messages
+from werkzeug.utils import secure_filename
+from database import User as us
+from database import Inventory as it
+from database import ausleihung as au
+from bson.objectid import ObjectId
+import hashlib
+import datetime
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+
+@app.route('/test_connection', methods=['GET'])
+def test_connection():
+    return {'status': 'success', 'message': 'Connection successful', 'version': __version__, 'status_code': 200}
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not username or not password:
+            flash('Please fill all fields', 'error')
+            get_flashed_messages()
+            return redirect(url_for('login'))
+        
+        user_instance = us()
+        user = user_instance.check_nm_pwd(username, password)
+
+        return render_template('home.html')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/register_new_user', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not username or not password:
+            flash('Please fill all fields', 'error')
+            Flask.get_flash_messages()
+            return redirect(url_for('register'))
+        if us.get_user(username):
+            flash('User already exists', 'error')
+            return redirect(url_for('register'))
+        if not us.check_password_strength(password):
+            flash('Password is too weak', 'error')
+            return redirect(url_for('register'))
+        us.add_user(username, password)
+        session['username'] = username
+        return redirect(url_for('home'))
+    return render_template('register.html')
